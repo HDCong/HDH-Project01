64ca99d3ac071b000dd988aadf9f8fb952c9bd0f
Cong Hoang <hoangduccong85@gmail.com>
2019-09-25 12:27:22 +0700

    excuting command and history command features

--- source.c
+++ source.c
@@ -3,24 +3,61 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define LINE_MAX 80
 #define TOK_BUF 45
 
-char* getLine(){
-    char *line = malloc(sizeof(char)*LINE_MAX);
-    char c;
-    int i;
-    for(i = 0; i < LINE_MAX && (c = getchar()) != EOF && c != '\n'; i++) {
-        line[i] = c;
-    }
-    line[i + 1] = '\0';
-    return line;
+void msg_promt(){
+    char buff[] = "osh> ";
+    printf("%s", buff);
+    fflush(stdout);
 }
 
-char **getTokens(char *temp){
+int getLenOfArgs(char **args){
+    int count = 0;
+    for(count; args[count] != NULL; count++);
+    return count;
+}
+
+
+char* getLine(){
+    char* line = malloc(sizeof(char) * LINE_MAX);
+    char c;
+    int bufSize = LINE_MAX;
+    int i = 0;
+    
+    while(1){
+        
+        c = getchar();
+        
+        if ( c == EOF || c == '\n')
+        {
+            line[i] = '\0';
+            return line;
+        }
+        else
+        {
+            line[i] = c;
+        }
+        
+        i += 1;
+        
+        if (i >= bufSize)
+        {
+            bufSize += LINE_MAX;
+            line = realloc(line, bufSize);
+        }
+    }
+}
+
+char **getTokens(char *temp, bool *isRunBackground) {
     char *string = malloc(sizeof(char) * (strlen(temp)+1));
     strcpy(string,temp);
+    if (string[strlen(string)-1]=='&') {
+        *isRunBackground=true;
+        string[strlen(string)-1]='\0';
+    }
     int bufsize = TOK_BUF, position = 0;
     char **tokens = (char**) malloc(bufsize * sizeof(char*));
     char *token;
@@ -30,13 +67,22 @@ char **getTokens(char *temp){
         position++;
         token = strtok(NULL, " |\t\r\n\a");
     }
+    
     tokens[position] = NULL;
+    
     return tokens;
 }
-
-void excutingCommand(char** args){
-    
+void deallocateMemory(char ** args){
+    int len = getLenOfArgs(args);
+    for(int i=0;i< len;i++)
+        free(args[i]);
+    free(args);
 }
+void excutingCommand(char** args){
+    execvp(args[0], args);
+    exit(EXIT_SUCCESS);
+}
+
 void historyFeature(char *historyCmd){
     if(!historyCmd){
         printf("No commands in history.\n");
@@ -51,31 +97,42 @@ int main(){
     char **args = NULL;
     char *historyCommand = NULL;
     while(status == 1){
-        printf("osh> ");
+        msg_promt();
         char *input_line = getLine();
-        args = getTokens(input_line);
-        //pid_t pid = fork();
-
-        // if(pid == -1){
-        //     printf("Child process could not be createld!\n");
-        // }
-        // else if(pid == 0){
-        //     excutingCommand(args);
-        // }
-        // else if(pid > 0){
-
-        // }
+        bool isRunBackground = false;
+        args = getTokens(input_line, &isRunBackground);
+        
+        if(args[0] == NULL){
+            continue;
+        }
+        if (!strcmp(args[0], "exit"))
+            status = 0;
         if(!strcmp(args[0],"!!")){
             historyFeature(historyCommand);
-            
         }
         else {
             free(historyCommand);
             historyCommand = malloc(sizeof(char) * (strlen(input_line)+1));
             strcpy(historyCommand, input_line);
+            pid_t pid;
+            
+            switch(pid = fork()) {
+                case -1:
+                    printf("Child process could not be created!\n");
+                    exit(EXIT_FAILURE);
+                    break;
+                case 0:
+                    excutingCommand(args);
+                    break;
+                default:
+                    if(!isRunBackground)
+                        wait(NULL);
+                    break;
+            }
         }
+        
         free(input_line);
     }
- 
+    deallocateMemory(args);
     return 0;
 }
